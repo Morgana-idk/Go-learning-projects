ea040ffdb39667f272956a51a47c17967c165aa8
Morgana-idk <pedrokook123p@gmail.com>
2026-04-23 20:15:56 -0300

    refatora estrutura de dados para uso de structs e simplifica lógica de exibição

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-	nomes := []string{
-		"Pedro",
-		"Davi",
-		"Alysson",
-		"Nicole",
-	}
-	nomes = append(nomes, "Não sei")
-	for _, nome := range nomes {
-		fmt.Println((nome))
-	}
-	fmt.Println("brutal")
+type pessoas []struct {
+	nome  string
+	forca int
 }
-d
+
+func main() {
+	pessoasMain := pessoas{
+		{nome: "Pedro", forca: 90},
+		{nome: "Davizin", forca: 90},
+		{nome: "Alysson", forca: 85},
+		{nome: "Nicole", forca: 100},
+		{nome: "Modulescript", forca: 1000000000000000000},
+	}
+
+	for _, pessoa := range pessoasMain {
+		if pessoa.forca >= 90 {
+			if pessoa.nome == "Pedro" {
+				fmt.Println("[Master] --", pessoa.nome, "[RIVAL] --", pessoa.forca)
+			} else {
+				fmt.Println("[Master] --", pessoa.nome, "--", pessoa.forca)
+			}
+		} else {
+			if pessoa.nome == "Pedro" {
+				fmt.Println("[Master] --", pessoa.nome, "[RIVAL] --", pessoa.forca)
+			} else {
+				fmt.Println("[Grinding] --", pessoa.nome, "--", pessoa.forca)
+			}
+		}
+	}
+}
